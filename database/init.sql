a00ded04999813e68d42c826cf760e7488a32e5f
louis.michault <louis.michault@etu.umontpellier.fr>
2026-04-30 17:12:12 +0200

    chore(database): add container id for applications and services

--- database/init.sql
+++ database/init.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS applications (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(50) NOT NULL,
     image_id varchar(12) NULL,
+    container_id varchar(12) NULL,
     created_at TIMESTAMP NOT NULL DEFAULT now()
 );
 
@@ -12,6 +13,7 @@ CREATE TABLE IF NOT EXISTS services (
     name VARCHAR(50) NOT NULL,
     version VARCHAR(12) NOT NULL,
     image_id varchar(12) NULL,
+    container_id varchar(12) NULL,
     created_at TIMESTAMP NOT NULL DEFAULT now()
 );
 
